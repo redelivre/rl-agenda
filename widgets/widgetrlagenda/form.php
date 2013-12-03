ad252a404d906064f1eb01d7ee06ca2b878bb61e
Flávio Zavan <flaviozavan@gmail.com>
2013-12-03 14:52:38 -0200

    Desquebra o form

--- widgets/widgetrlagenda/form.php
+++ widgets/widgetrlagenda/form.php
@@ -1,22 +1,12 @@
-<?php
-
-function form($instance) {
-		$title = isset($instance['title']) ? $instance['title'] : '';
-		$num_posts = isset($instance['num_posts']) ? $instance['num_posts'] : '';
-		?>
-		<p>
-				<label for="<?php echo $this->get_field_id('title'); ?>">
-						<?php _e('Título'); ?><br/>
-						<input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo isset($title) ? $title : 'Agenda'; ?>"/>
-				</label>
-		</p>
-		<p>
-				<label for="<?php echo $this->get_field_id('post_name'); ?>">
-						<?php _e('Número de eventos na lista'); ?> 
-						<input class="widefat" id="<?php echo $this->get_field_id('num_posts'); ?>" name="<?php echo $this->get_field_name('num_posts'); ?>" type="text" value="<?php echo $num_posts; ?>" />
-				</label>
-		</p>
-		<?php
-}
-
-?>
+<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>">
+				<?php _e('Título'); ?><br/>
+				<input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo isset($title) ? $title : 'Agenda'; ?>"/>
+		</label>
+</p>
+<p>
+		<label for="<?php echo $this->get_field_id('post_name'); ?>">
+				<?php _e('Número de eventos na lista'); ?> 
+				<input class="widefat" id="<?php echo $this->get_field_id('num_posts'); ?>" name="<?php echo $this->get_field_name('num_posts'); ?>" type="text" value="<?php echo $num_posts; ?>" />
+		</label>
+</p>
